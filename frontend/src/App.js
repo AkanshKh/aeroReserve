21ceb3b4339197ce305052125a14713c437a5c3c
Akansh Khandelwal <128902296+AkanshKh@users.noreply.github.com>
2024-06-18 21:06:08 +0530

    made book component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Home from './components/home';
 import Login from './components/login';
 import SignUp from './components/signUp';
 import Flight from './components/flight';
+import Book from './components/book';
 const App = () => {
     return (
         <Router>
@@ -12,6 +13,7 @@ const App = () => {
                     <Route path="/login" element={<Login />} />
                     <Route path="/signup" element={<SignUp />} />
                     <Route path="/flight" element={<Flight />} />
+                    <Route path="/book" element={<Book />} />
                     {/* Add more routes as needed */}
                 </Routes>
         </Router>
